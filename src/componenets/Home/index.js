a83fe0712d4aad1cea1b25229cdb9db06c6547ed
amanshawar <amanshawar_a@hotmail.com>
2019-01-30 10:50:29 +0200

    update gitignore

---
+++ src/componenets/Home/index.js
@@ -0,0 +1,31 @@
+import React,{Component} from 'react';
+import Header from "../Header/index.js";
+import StartPlay from '../StartPlay/index.js';
+
+class Home extends Component {
+  state = {
+  firstName: '',
+  secondName:'',
+  submitted:false
+}
+
+handleChange=({ target: { value, name } })=> {
+  this.setState({ [name]: value });
+}
+startplay=(e)=> {
+  e.preventDefault();
+  this.setState({ submitted:true });
+
+}
+
+render(){
+if (!this.state.submitted) {
+  return(<StartPlay handleChange={this.handleChange} startplay={this.startplay}/>)
+}else{
+  return (<Header/>)
+}
+
+
+}
+}
+export default Home;
