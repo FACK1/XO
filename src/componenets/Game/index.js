7763343b09483b37a2ee476c3c52d1fb6fdb8659
amanshawar <amanshawar_a@hotmail.com>
2019-01-30 20:46:11 +0200

    play the game #3

--- src/componenets/Game/index.js
+++ src/componenets/Game/index.js
@@ -3,67 +3,51 @@ import React, {Component} from "react";
 import "./index.css";
 class Game extends Component {
   state = {
-    indexX:[],
-    indexY:[],
     turn :"X",
+    game:Array(9).fill('')
   };
-
     press = (e) => {
-      console.log('sssss',e.target);
-      if (e.target.value >=0 || e.target.value <=8) {
-        e.innerText = this.state.turn;
-      if (this.state.turn==="X") {
-      this.setState({turn:"Y" });
+      if (this.state.game[e.dataset.value]=='') {
 
-      e.target.value=this.state.turn;
-      // console.log(this.state.turn);
-      this.setState({indexX:[...this.state.indexX, e.target.value]});
-    }else {
-      this.setState({turn:"X" });
-          // console.log(this.state.turn);
-      this.setState({indexY:[...this.state.indexY, e.target.value]});
-    }
-  }else {
-    return;
+      this.state.game[e.dataset.value]=this.state.turn;
+      e.innerText=this.state.turn;
+
+      this.setState({
+        turn: this.state.turn == 'X' ? 'O' : 'X',
+        game:this.state.game
+    });
   }
+    var theWinner=this.win();
+    console.log(theWinner);
     };
+    win = () => {
+      var moves = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]];
+      var game = this.state.game;
+ for(let i=0;i<moves.length;i++) {
+   if(game[moves[i][0]] == game[moves[i][1]] && game[moves[i][1]] == game[moves[i][2]])
+       return game[moves[i][0]];
+ }
+    };
+
   render(){
 
     return (
       <div>
-      <div className="row">
-        <div className="square">
-          <input type="button" value="0" onClick={this.press}/>
+          <div id="board" onClick={(e)=>this.press(e.target)}>
+
+                  <div className="square" data-value="0"></div>
+                  <div className="square" data-value="1"></div>
+                  <div className="square" data-value="2"></div>
+
+                  <div className="square row" data-value="3"></div>
+                  <div className="square" data-value="4"></div>
+                  <div className="square" data-value="5"></div>
+
+                  <div className="square row" data-value="6"></div>
+                  <div className="square" data-value="7"></div>
+                  <div className="square" data-value="8"></div>
+
         </div>
-        <div className="square">
-          <input type="button" value="1" onClick={this.press}/>
-        </div>
-        <div className="square">
-          <input type="button" value="2" onClick={this.press}/>
-        </div>
-      </div>
-      <div className="row">
-        <div className="square">
-          <input type="button" value="3" onClick={this.press}/>
-        </div>
-        <div className="square">
-          <input type="button" value="4" onClick={this.press}/>
-        </div>
-        <div className="square">
-          <input type="button" value="5" onClick={this.press}/>
-        </div>
-      </div>
-      <div className="row">
-        <div className="square">
-          <input type="button" value="6" onClick={this.press}/>
-        </div>
-        <div className="square">
-          <input type="button" value="7" onClick={this.press}/>
-        </div>
-        <div className="square">
-          <input type="button" value="8" onClick={this.press}/>
-        </div>
-      </div>
       </div>
     );
   }
